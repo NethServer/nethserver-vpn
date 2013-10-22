3d3df0622f96013ad509138782fc5e348d1e5dff
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-10-22 15:37:57 +0200

    web ui: update translations. Refs #1763

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_VPN.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_VPN.php
@@ -2,3 +2,5 @@
 
 $L['VPN_Title'] = 'VPN';
 $L['VPN_Tags'] = 'VPN vpn virtual private network';
+$L['VPN_Description'] = 'Configura server e client VPN';
+$L['VPN_Tags'] = 'vpn VPN vpns net2net roadwarrior nettonet';
