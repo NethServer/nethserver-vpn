43d7acdb6914999e0922227e86c301ca80c29224
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2015-09-25 10:43:43 +0200

    Remove Italian translation. Refs #3265

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_VPN.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$L['VPN_Title'] = 'VPN';
-$L['VPN_Tags'] = 'VPN vpn virtual private network';
-$L['VPN_Description'] = 'Configura server e client VPN';
-$L['VPN_Tags'] = 'vpn VPN vpns net2net roadwarrior nettonet';
